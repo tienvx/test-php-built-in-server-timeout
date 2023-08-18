4dc8d25558ea02a8cf8e88fa3d33f65c6f6f5e89
tienvx <tien.xuan.vo@gmail.com>
2023-08-18 10:08:31 +0700

    Clean up src

--- src/ffi-first.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-require(__DIR__ . '/ffi-verifier.php');
-require(__DIR__ . '/stub-server-standalone.php');
--- src/standalone-first.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-require(__DIR__ . '/stub-server-standalone.php');
-require(__DIR__ . '/ffi-verifier.php');
